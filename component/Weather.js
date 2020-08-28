099983f917a0b4f0b9774fa4cbbc76d99de6bcfb
Aorts <gusgame.aor@gmail.com>
2020-08-29 02:12:52 +0700

    3rd commit

--- component/Weather.js
+++ component/Weather.js
@@ -1,18 +1,38 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Text, ImageBackground, StyleSheet } from 'react-native'
 import Forecast from './Forecast'
 
 export default function Weather(props) {
+
     const [forecastInfo, setForecastInfo] = useState({
         main: '-',
         description: '-',
         temp: 0
     }) 
 
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=7fa6f3c7fd9ae8ab3881a795ecbbe17c`)
+        .then((response) => response.json())
+        .then((json) => {
+            setForecastInfo({
+                main: json.weather[0].main,
+                description: json.weather[0].description,
+                temp: json.main.temp});
+             })
+        .catch((error) => {
+             console.warn(error);
+         });
+        }
+     }, [props.zipCode])
+
+    
+
     return (
         <ImageBackground source={require('../bg.jpg')} style={style.backdrop}> 
-           <Text>Zip code</Text>
-           <Text>{props.zipcode}</Text>
+           <Text>Zipcode</Text>
+           <Text>{props.zipCode}</Text>
            <Forecast {...forecastInfo}/> 
         </ImageBackground>
     
--- component/tempCodeRunnerFile.js
+++ component/tempCodeRunnerFile.js
@@ -1,1 +1,16 @@
-StyleSheet
+useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=7fa6f3c7fd9ae8ab3881a795ecbbe17c`)
+        .then((response) => response.json())
+        .then((json) => {
+            setForecastInfo({
+                main: json.weather[0].main,
+                description: json.weather[0].description,
+                temp: json.main.temp});
+             })
+        .catch((error) => {
+             console.warn(error);
+         });
+        }
+     }, [props.zipCode])
